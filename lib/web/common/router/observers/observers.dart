245da186b4cb5ed0b0de6ef1f84a87deaa18d411
ericFlutterast <erikGitFlutter@gmail.com>
2024-07-09 11:58:23 +0300

    created observers

---
+++ lib/web/common/router/observers/observers.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+final class PageObserver extends RouteObserver<PageRoute<Object?>> {}
+
+final class ModalObserver extends RouteObserver<ModalRoute<Object?>> {}
